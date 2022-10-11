c29c9098d54193d092e7abb08cfd5206384f66f6
azfang <47017488+azfang@users.noreply.github.com>
2022-10-10 21:57:42 -0400

    Update main.js

--- main.js
+++ main.js
@@ -23,8 +23,8 @@
         function ScrollDiv2() {
 
             if (document.getElementById('autoscrollmotion2').scrollTop < (document.getElementById('autoscrollmotion2').scrollHeight - document.getElementById('autoscrollmotion2').offsetHeight)) {
-                -1
-                document.getElementById('autoscrollmotion2').scrollTop = document.getElementById('autoscrollmotion2').scrollTop + 1
+                +1
+                document.getElementById('autoscrollmotion2').scrollTop = document.getElementById('autoscrollmotion2').scrollTop - 1
             }
             else { document.getElementById('autoscrollmotion2').scrollTop = 0; }
         }
@@ -47,7 +47,67 @@
             else { document.getElementById('autoscrollmotion4').scrollTop = 0; }
         }
 
+        function ScrollDiv5() {
+
+            if (document.getElementById('autoscrollmotion5').scrollTop < (document.getElementById('autoscrollmotion5').scrollHeight - document.getElementById('autoscrollmotion5').offsetHeight)) {
+                -1
+                document.getElementById('autoscrollmotion5').scrollTop = document.getElementById('autoscrollmotion5').scrollTop + 1
+            }
+            else { document.getElementById('autoscrollmotion5').scrollTop = 0; }
+        }
+
+        function ScrollDiv6() {
+
+            if (document.getElementById('autoscrollmotion6').scrollTop < (document.getElementById('autoscrollmotion6').scrollHeight - document.getElementById('autoscrollmotion6').offsetHeight)) {
+                -1
+                document.getElementById('autoscrollmotion6').scrollTop = document.getElementById('autoscrollmotion6').scrollTop + 1
+            }
+            else { document.getElementById('autoscrollmotion6').scrollTop = 0; }
+        }
+
+        function ScrollDiv7() {
+
+            if (document.getElementById('autoscrollmotion7').scrollTop < (document.getElementById('autoscrollmotion7').scrollHeight - document.getElementById('autoscrollmotion7').offsetHeight)) {
+                -1
+                document.getElementById('autoscrollmotion7').scrollTop = document.getElementById('autoscrollmotion7').scrollTop + 1
+            }
+            else { document.getElementById('autoscrollmotion7').scrollTop = 0; }
+        }
+
+        function ScrollDiv8() {
+
+            if (document.getElementById('autoscrollmotion8').scrollTop < (document.getElementById('autoscrollmotion8').scrollHeight - document.getElementById('autoscrollmotion8').offsetHeight)) {
+                -1
+                document.getElementById('autoscrollmotion8').scrollTop = document.getElementById('autoscrollmotion8').scrollTop + 1
+            }
+            else { document.getElementById('autoscrollmotion8').scrollTop = 0; }
+        }
+
+        function ScrollDiv9() {
+
+            if (document.getElementById('autoscrollmotion9').scrollTop < (document.getElementById('autoscrollmotion9').scrollHeight - document.getElementById('autoscrollmotion9').offsetHeight)) {
+                -1
+                document.getElementById('autoscrollmotion9').scrollTop = document.getElementById('autoscrollmotion9').scrollTop + 1
+            }
+            else { document.getElementById('autoscrollmotion9').scrollTop = 0; }
+        }
+
+        function ScrollDiv10() {
+
+            if (document.getElementById('autoscrollmotion10').scrollTop < (document.getElementById('autoscrollmotion10').scrollHeight - document.getElementById('autoscrollmotion10').offsetHeight)) {
+                -1
+                document.getElementById('autoscrollmotion10').scrollTop = document.getElementById('autoscrollmotion10').scrollTop + 1
+            }
+            else { document.getElementById('autoscrollmotion10').scrollTop = 0; }
+        }
+
         setInterval(ScrollDiv, 50)
         setInterval(ScrollDiv2, 50)
         setInterval(ScrollDiv3, 50)
         setInterval(ScrollDiv4, 50)
+        setInterval(ScrollDiv5, 50)
+        setInterval(ScrollDiv6, 50)
+        setInterval(ScrollDiv7, 50)
+        setInterval(ScrollDiv8, 50)
+        setInterval(ScrollDiv9, 50)
+        setInterval(ScrollDiv10, 50)
