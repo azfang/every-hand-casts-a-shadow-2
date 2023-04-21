eda0711fe26213cbf66f2c9a25748d27ee6360f9
azfang <47017488+azfang@users.noreply.github.com>
2023-04-21 11:53:26 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,398 +1,248 @@
-        function ScrollDiv() {
+//Automatic scrolling of the sections 
 
-                if (document.getElementById('autoscrollmotion1').scrollTop < (document.getElementById('autoscrollmotion1').scrollHeight - document.getElementById('autoscrollmotion1').offsetHeight)) {                
-                    -1
-                    document.getElementById('autoscrollmotion1').scrollTop = document.getElementById('autoscrollmotion1').scrollTop + 1
-                }
-                else { document.getElementById('autoscrollmotion1').scrollTop = 0; }
-            }
-    
-            function ScrollDiv2() {
-    
-                if (document.getElementById('autoscrollmotion2').scrollTop < (document.getElementById('autoscrollmotion2').scrollHeight - document.getElementById('autoscrollmotion2').offsetHeight)) {
-                    -1
-                    document.getElementById('autoscrollmotion2').scrollTop = document.getElementById('autoscrollmotion2').scrollTop + 1
-                }
-                else { document.getElementById('autoscrollmotion2').scrollTop = 0; }
-            }
-    
-            function ScrollDiv3() {
-    
-                if (document.getElementById('autoscrollmotion3').scrollTop < (document.getElementById('autoscrollmotion3').scrollHeight - document.getElementById('autoscrollmotion3').offsetHeight)) {
-                    -1
-                    document.getElementById('autoscrollmotion3').scrollTop = document.getElementById('autoscrollmotion3').scrollTop + 1
-                }
-                else { document.getElementById('autoscrollmotion3').scrollTop = 0; }
-            }
-    
-            function ScrollDiv4() {
-    
-                if (document.getElementById('autoscrollmotion4').scrollTop < (document.getElementById('autoscrollmotion4').scrollHeight - document.getElementById('autoscrollmotion4').offsetHeight)) {
-                    -1
-                    document.getElementById('autoscrollmotion4').scrollTop = document.getElementById('autoscrollmotion4').scrollTop + 1
-                }
-                else { document.getElementById('autoscrollmotion4').scrollTop = 0; }
-            }
-    
-            function ScrollDiv5() {
-    
-                if (document.getElementById('autoscrollmotion5').scrollTop < (document.getElementById('autoscrollmotion5').scrollHeight - document.getElementById('autoscrollmotion5').offsetHeight)) {
-                    -1
-                    document.getElementById('autoscrollmotion5').scrollTop = document.getElementById('autoscrollmotion5').scrollTop + 1
-                }
-                else { document.getElementById('autoscrollmotion5').scrollTop = 0; }
-            }
-    
-            function ScrollDiv6() {
-    
-                if (document.getElementById('autoscrollmotion6').scrollTop < (document.getElementById('autoscrollmotion6').scrollHeight - document.getElementById('autoscrollmotion6').offsetHeight)) {
-                    -1
-                    document.getElementById('autoscrollmotion6').scrollTop = document.getElementById('autoscrollmotion6').scrollTop + 1
-                }
-                else { document.getElementById('autoscrollmotion6').scrollTop = 0; }
-            }
-    
-            function ScrollDiv7() {
-    
-                if (document.getElementById('autoscrollmotion7').scrollTop < (document.getElementById('autoscrollmotion7').scrollHeight - document.getElementById('autoscrollmotion7').offsetHeight)) {
-                    -1
-                    document.getElementById('autoscrollmotion7').scrollTop = document.getElementById('autoscrollmotion7').scrollTop + 1
-                }
-                else { document.getElementById('autoscrollmotion7').scrollTop = 0; }
-            }
-    
-            function ScrollDiv8() {
-    
-                if (document.getElementById('autoscrollmotion8').scrollTop < (document.getElementById('autoscrollmotion8').scrollHeight - document.getElementById('autoscrollmotion8').offsetHeight)) {
-                    -1
-                    document.getElementById('autoscrollmotion8').scrollTop = document.getElementById('autoscrollmotion8').scrollTop + 1
-                }
-                else { document.getElementById('autoscrollmotion8').scrollTop = 0; }
-            }
-    
-            function ScrollDiv9() {
-    
-                if (document.getElementById('autoscrollmotion9').scrollTop < (document.getElementById('autoscrollmotion9').scrollHeight - document.getElementById('autoscrollmotion9').offsetHeight)) {
-                    -1
-                    document.getElementById('autoscrollmotion9').scrollTop = document.getElementById('autoscrollmotion9').scrollTop + 1
-                }
-                else { document.getElementById('autoscrollmotion9').scrollTop = 0; }
-            }
-    
-            function ScrollDiv10() {
-    
-                if (document.getElementById('autoscrollmotion10').scrollTop < (document.getElementById('autoscrollmotion10').scrollHeight - document.getElementById('autoscrollmotion10').offsetHeight)) {
-                    -1
-                    document.getElementById('autoscrollmotion10').scrollTop = document.getElementById('autoscrollmotion10').scrollTop + 1
-                }
-                else { document.getElementById('autoscrollmotion10').scrollTop = 0; }
-            }
-    
-            setInterval(ScrollDiv, 50)
-            setInterval(ScrollDiv2, 50)
-            setInterval(ScrollDiv3, 50)
-            setInterval(ScrollDiv4, 50)
-            setInterval(ScrollDiv5, 50)
-            setInterval(ScrollDiv6, 50)
-            setInterval(ScrollDiv7, 50)
-            setInterval(ScrollDiv8, 50)
-            setInterval(ScrollDiv9, 50)
-            setInterval(ScrollDiv10, 50)
+function ScrollDiv() {
 
-            var items;
+  if (document.getElementById('autoscrollmotion1').scrollTop < (document.getElementById('autoscrollmotion1').scrollHeight - document.getElementById('autoscrollmotion1').offsetHeight)) {
+    -1
+    document.getElementById('autoscrollmotion1').scrollTop = document.getElementById('autoscrollmotion1').scrollTop + 1
+  }
+  else { document.getElementById('autoscrollmotion1').scrollTop = 0; }
+}
 
-$(document).ready(function() {
-    //homepage
+function ScrollDiv2() {
 
-    //homepage popup animations
-    $("#r1quote").hide();
+  if (document.getElementById('autoscrollmotion2').scrollTop < (document.getElementById('autoscrollmotion2').scrollHeight - document.getElementById('autoscrollmotion2').offsetHeight)) {
+    +1
+    document.getElementById('autoscrollmotion2').scrollTop = document.getElementById('autoscrollmotion2').scrollTop + 1
+  }
+  else { document.getElementById('autoscrollmotion2').scrollTop = 0; }
+}
+
+function ScrollDiv3() {
+
+  if (document.getElementById('autoscrollmotion3').scrollTop < (document.getElementById('autoscrollmotion3').scrollHeight - document.getElementById('autoscrollmotion3').offsetHeight)) {
+    -1
+    document.getElementById('autoscrollmotion3').scrollTop = document.getElementById('autoscrollmotion3').scrollTop + 1
+  }
+  else { document.getElementById('autoscrollmotion3').scrollTop = 0; }
+}
+
+function ScrollDiv4() {
+
+  if (document.getElementById('autoscrollmotion4').scrollTop < (document.getElementById('autoscrollmotion4').scrollHeight - document.getElementById('autoscrollmotion4').offsetHeight)) {
+    -1
+    document.getElementById('autoscrollmotion4').scrollTop = document.getElementById('autoscrollmotion4').scrollTop + 1
+  }
+  else { document.getElementById('autoscrollmotion4').scrollTop = 0; }
+}
+
+function ScrollDiv5() {
+
+  if (document.getElementById('autoscrollmotion5').scrollTop < (document.getElementById('autoscrollmotion5').scrollHeight - document.getElementById('autoscrollmotion5').offsetHeight)) {
+    -1
+    document.getElementById('autoscrollmotion5').scrollTop = document.getElementById('autoscrollmotion5').scrollTop + 1
+  }
+  else { document.getElementById('autoscrollmotion5').scrollTop = 0; }
+}
+
+function ScrollDiv6() {
+
+  if (document.getElementById('autoscrollmotion6').scrollTop < (document.getElementById('autoscrollmotion6').scrollHeight - document.getElementById('autoscrollmotion6').offsetHeight)) {
+    -1
+    document.getElementById('autoscrollmotion6').scrollTop = document.getElementById('autoscrollmotion6').scrollTop + 1
+  }
+  else { document.getElementById('autoscrollmotion6').scrollTop = 0; }
+}
+
+function ScrollDiv7() {
+
+  if (document.getElementById('autoscrollmotion7').scrollTop < (document.getElementById('autoscrollmotion7').scrollHeight - document.getElementById('autoscrollmotion7').offsetHeight)) {
+    -1
+    document.getElementById('autoscrollmotion7').scrollTop = document.getElementById('autoscrollmotion7').scrollTop + 1
+  }
+  else { document.getElementById('autoscrollmotion7').scrollTop = 0; }
+}
+
+function ScrollDiv8() {
+
+  if (document.getElementById('autoscrollmotion8').scrollTop < (document.getElementById('autoscrollmotion8').scrollHeight - document.getElementById('autoscrollmotion8').offsetHeight)) {
+    -1
+    document.getElementById('autoscrollmotion8').scrollTop = document.getElementById('autoscrollmotion8').scrollTop + 1
+  }
+  else { document.getElementById('autoscrollmotion8').scrollTop = 0; }
+}
+
+function ScrollDiv9() {
+
+  if (document.getElementById('autoscrollmotion9').scrollTop < (document.getElementById('autoscrollmotion9').scrollHeight - document.getElementById('autoscrollmotion9').offsetHeight)) {
+    -1
+    document.getElementById('autoscrollmotion9').scrollTop = document.getElementById('autoscrollmotion9').scrollTop + 1
+  }
+  else { document.getElementById('autoscrollmotion9').scrollTop = 0; }
+}
+
+function ScrollDiv10() {
+
+  if (document.getElementById('autoscrollmotion10').scrollTop < (document.getElementById('autoscrollmotion10').scrollHeight - document.getElementById('autoscrollmotion10').offsetHeight)) {
+    -1
+    document.getElementById('autoscrollmotion10').scrollTop = document.getElementById('autoscrollmotion10').scrollTop + 1
+  }
+  else { document.getElementById('autoscrollmotion10').scrollTop = 0; }
+}
+
+setInterval(ScrollDiv, 50)
+setInterval(ScrollDiv2, 50)
+setInterval(ScrollDiv3, 50)
+setInterval(ScrollDiv4, 50)
+setInterval(ScrollDiv5, 50)
+setInterval(ScrollDiv6, 50)
+setInterval(ScrollDiv7, 50)
+setInterval(ScrollDiv8, 50)
+setInterval(ScrollDiv9, 50)
+setInterval(ScrollDiv10, 50)
+
+var items;
+
+//Starting fade-in animation
+$(document).ready(function () {
+  $("#r1quote").hide();
+  $("#r2quote").hide();
+  $("#r3quote").hide();
+  fade1.play();
+  fade2.play();
+  fade3.play();
+  fade4.play();
+  fade5.play();
+  fade6.play();
+  fade7.play();
+  fade8.play();
+  fade9.play();
+  fade10.play();
 });
-var R1T1I1 = anime({
-    targets: '#loopvid1',
-    // translateY: [
-    //   {value: -25, duration: 400},
-    // ],
-    opacity: [
-      {value: 1, duration: 350},
-    ],
-    delay: 200,
-    easing: 'easeInOutQuad',
-    autoplay: false
-  });
-var R1T1B1 = anime({
-    targets: '#r1quote_block1',
-    // translateY: [
-    //   {value: -25, duration: 400},
-    // ],
-    opacity: [
-      {value: 1, duration: 350},
-    ],
-    delay: 300,
-    easing: 'easeInOutQuad',
-    autoplay: false
-  });
 
-var R1T1B2 = anime({
-    targets: '#r1quote_block2',
-    opacity: [
-      {value: 1, duration: 350},
-    ],
-    delay: 400,
-    easing: 'easeInOutQuad',
-    autoplay: false
-  });
+var fade1 = anime({
+  targets: '.row1',
+  // translateY: [
+  //   {value: -25, duration: 400},
+  // ],
+  opacity: [
+    { value: 1, duration: 350 },
+  ],
+  delay: 0,
+  easing: 'easeInOutQuad',
+  autoplay: false
+});
 
-var R1T1B3 = anime({
-    targets: '#r1quote_block3',
-    opacity: [
-      {value: 1, duration: 350},
-    ],
-    delay: 500,
-    easing: 'easeInOutQuad',
-    autoplay: false
-  });
+var fade2 = anime({
+  targets: '.row2',
+  // translateY: [
+  //   {value: -25, duration: 400},
+  // ],
+  opacity: [
+    { value: 1, duration: 350 },
+  ],
+  delay: 100,
+  easing: 'easeInOutQuad',
+  autoplay: false
+});
 
-var R1T1B4 = anime({
-    targets: '#r1quote_block4',
-    // translateY: [
-    //   {value: -25, duration: 500},
-    // ],
-    opacity: [
-      {value: 1, duration: 350},
-    ],
-    delay: 600,
-    easing: 'easeInOutQuad',
-    autoplay: false
-  });
+var fade3 = anime({
+  targets: '.row3',
+  // translateY: [
+  //   {value: -25, duration: 400},
+  // ],
+  opacity: [
+    { value: 1, duration: 350 },
+  ],
+  delay: 200,
+  easing: 'easeInOutQuad',
+  autoplay: false
+});
 
-var R1T1B5 = anime({
-    targets: '#r1quote_block5',
-    opacity: [
-      {value: 1, duration: 350},
-    ],
-    delay: 700,
-    easing: 'easeInOutQuad',
-    autoplay: false
-  });
+var fade4 = anime({
+  targets: '.row4',
+  // translateY: [
+  //   {value: -25, duration: 400},
+  // ],
+  opacity: [
+    { value: 1, duration: 350 },
+  ],
+  delay: 300,
+  easing: 'easeInOutQuad',
+  autoplay: false
+});
 
-var R1T1B6 = anime({
-    targets: '#r1quote_block6',
-    opacity: [
-      {value: 1, duration: 350},
-    ],
-    delay: 800,
-    easing: 'easeInOutQuad',
-    autoplay: false
-  });
+var fade5 = anime({
+  targets: '.row5',
+  // translateY: [
+  //   {value: -25, duration: 400},
+  // ],
+  opacity: [
+    { value: 1, duration: 350 },
+  ],
+  delay: 400,
+  easing: 'easeInOutQuad',
+  autoplay: false
+});
 
-var R1T1B7 = anime({
-    targets: '#r1quote_block7',
-    opacity: [
-      {value: 1, duration: 350},
-    ],
-    delay: 900,
-    easing: 'easeInOutQuad',
-    autoplay: false
-  });
+var fade6 = anime({
+  targets: '.row6',
+  // translateY: [
+  //   {value: -25, duration: 400},
+  // ],
+  opacity: [
+    { value: 1, duration: 350 },
+  ],
+  delay: 500,
+  easing: 'easeInOutQuad',
+  autoplay: false
+});
 
-var R1T1B8 = anime({
-    targets: '#r1quote_block8',
-    opacity: [
-      {value: 1, duration: 350},
-    ],
-    delay: 1000,
-    easing: 'easeInOutQuad',
-    autoplay: false
-  });
+var fade7 = anime({
+  targets: '.row7',
+  // translateY: [
+  //   {value: -25, duration: 400},
+  // ],
+  opacity: [
+    { value: 1, duration: 350 },
+  ],
+  delay: 600,
+  easing: 'easeInOutQuad',
+  autoplay: false
+});
 
-var R1T1B9 = anime({
-    targets: '#r1quote_block9',
-    opacity: [
-      {value: 1, duration: 350},
-    ],
-    delay: 1100,
-    easing: 'easeInOutQuad',
-    autoplay: false,
-  });
+var fade8 = anime({
+  targets: '.row8',
+  // translateY: [
+  //   {value: -25, duration: 400},
+  // ],
+  opacity: [
+    { value: 1, duration: 350 },
+  ],
+  delay: 700,
+  easing: 'easeInOutQuad',
+  autoplay: false
+});
 
-var R1T1I2 = anime({
-    targets: '#loopvid2',
-    opacity: [
-      {value: 1, duration: 350},
-    ],
-    delay: 1100,
-    easing: 'easeInOutQuad',
-    autoplay: false
-  });
+var fade9 = anime({
+  targets: '.row9',
+  // translateY: [
+  //   {value: -25, duration: 400},
+  // ],
+  opacity: [
+    { value: 1, duration: 350 },
+  ],
+  delay: 800,
+  easing: 'easeInOutQuad',
+  autoplay: false
+});
 
-var R1T1B10 = anime({
-    targets: '#r1quote_block10',
-    opacity: [
-      {value: 1, duration: 350},
-    ],
-    delay: 1100,
-    easing: 'easeInOutQuad',
-    autoplay: false,
-  });
-
-var R1T1B11 = anime({
-    targets: '#r1quote_block11',
-    opacity: [
-      {value: 1, duration: 350},
-    ],
-    delay: 1200,
-    easing: 'easeInOutQuad',
-    autoplay: false,
-  });
-
-var R1T1B12 = anime({
-    targets: '#r1quote_block12',
-    opacity: [
-      {value: 1, duration: 350},
-    ],
-    delay: 1300,
-    easing: 'easeInOutQuad',
-    autoplay: false,
-  });
-var R1T1B13 = anime({
-    targets: '#r1quote_block13',
-    opacity: [
-      {value: 1, duration: 500},
-    ],
-    delay: 1400,
-    easing: 'easeInOutQuad',
-    autoplay: false,
-  });
-var R1T1B14 = anime({
-    targets: '#r1quote_block14',
-    opacity: [
-      {value: 1, duration: 350},
-    ],
-    delay: 1500,
-    easing: 'easeInOutQuad',
-    autoplay: false,
-  });
-var R1T1B15 = anime({
-    targets: '#r1quote_block15',
-    opacity: [
-      {value: 1, duration: 350},
-    ],
-    delay: 1600,
-    easing: 'easeInOutQuad',
-    autoplay: false,
-  });
-var R1T1B16 = anime({
-    targets: '#r1quote_block16',
-    opacity: [
-      {value: 1, duration: 350},
-    ],
-    delay: 1700,
-    easing: 'easeInOutQuad',
-    autoplay: false,
-  });
-var R1T1B17 = anime({
-    targets: '#r1quote_block17',
-    opacity: [
-      {value: 1, duration: 350},
-    ],
-    delay: 1800,
-    easing: 'easeInOutQuad',
-    autoplay: false,
-  });
-var R1T1B18 = anime({
-    targets: '#r1quote_block18',
-    opacity: [
-      {value: 1, duration: 350},
-    ],
-    delay: 1800,
-    easing: 'easeInOutQuad',
-    autoplay: false,
-  });
-var R1T1B19 = anime({
-    targets: '#r1quote_block19',
-    opacity: [
-      {value: 1, duration: 350},
-    ],
-    delay: 1900,
-    easing: 'easeInOutQuad',
-    autoplay: false,
-  });
-var R1T1B20 = anime({
-    targets: '#r1quote_block20',
-    opacity: [
-      {value: 1, duration: 350},
-    ],
-    delay: 2000,
-    easing: 'easeInOutQuad',
-    autoplay: false,
-  });
-var R1T1B21 = anime({
-    targets: '#r1quote_block21',
-    opacity: [
-      {value: 1, duration: 350},
-    ],
-    delay: 2100,
-    easing: 'easeInOutQuad',
-    autoplay: false,
-  });
-var R1T1I3 = anime({
-    targets: '#loopvid3',
-    // translateY: [
-    //   {value: -25, duration: 400},
-    // ],
-    opacity: [
-      {value: 1, duration: 350},
-    ],
-    delay: 2200,
-    easing: 'easeInOutQuad',
-    autoplay: false
-  });
-
-$(".row1").click(quote1appears);
-function quote1appears() {
-    $("#r1quote").fadeIn();
-    // document.getElementById("r1quote").style.zIndex = "-1";
-    R1T1I1.play();
-    R1T1B1.play();
-    R1T1B2.play();
-    R1T1B3.play();
-    R1T1B4.play();
-    R1T1B5.play();
-    R1T1B6.play();
-    R1T1B7.play();
-    R1T1B8.play();
-    R1T1B9.play();
-    R1T1I2.play();
-    R1T1B10.play();
-    R1T1B11.play();
-    R1T1B12.play();
-    R1T1B13.play();
-    R1T1B14.play();
-    R1T1B15.play();
-    R1T1B16.play();
-    R1T1B17.play();
-    R1T1B18.play();
-    R1T1B19.play();
-    R1T1B20.play();
-    R1T1B21.play();
-    R1T1I3.play();
-    setTimeout(function() { IndexTest() },12000);
-    setTimeout(function() { R1QuoteFade() },12000);
-}
-
-function IndexTest() {
-    document.getElementById("r1quote").style.zIndex = "-1";
-}
-
-function R1QuoteFade() {
-    R1T1fade.play();
-}
-
-var R1T1fade = anime({
-    targets: '#r1quote',
-    opacity: [
-      {value: 0.15, duration: 500},
-    ],
-    easing: 'easeInOutQuad',
-    autoplay: false
-  });
+var fade10 = anime({
+  targets: '.row10',
+  opacity: [
+    { value: 1, duration: 350 },
+  ],
+  delay: 800,
+  easing: 'easeInOutQuad',
+  autoplay: false
+});
